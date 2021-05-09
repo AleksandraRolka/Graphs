1dbb3cce7042d155fc41f8f3a6203b9791c8afdb
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2021-05-09 21:12:10 +0200

    bugfix

--- project03/task05.py
+++ project03/task05.py
@@ -2,13 +2,13 @@ from task01 import *
 from utils import *
 import numpy as np
 
-def union(a, b, sets):
-    sets[a] = b
+def union(a, b, subsets):
+    subsets[a] = b
 
-def find_set(v, sets):
-    if sets[v] == v:
+def find_set(v, subsets):
+    if subsets[v] == v:
         return v
-    return find_set(sets[v], sets)
+    return find_set(subsets[v], subsets)
 
 def kruskal_algorithm(graph):
 
@@ -24,32 +24,35 @@ def kruskal_algorithm(graph):
             if graph[i][j] > 0:
                 list_of_edges.append({"weight": graph[i][j], "index": [i, j]})
 
-    sets = []
+    subsets = []
 
     for i in range(n):
-        sets.append(i)
+        subsets.append(i)
 
     list_of_edges.sort(key = lambda x : x["weight"])
 
     placed_edges = 0
+    sum_of_weights = 0
+
     for edge in list_of_edges:
 
         # get index of vertices
         i = edge["index"][0]
         j = edge["index"][1]
 
-        i_set = find_set(i, sets)
-        j_set = find_set(j, sets)
+        i_set = find_set(i, subsets)
+        j_set = find_set(j, subsets)
 
         if i_set != j_set:
             minimal_spanning_tree[i][j] = minimal_spanning_tree[j][i] = edge["weight"]
-            union(i_set, j_set, sets)
+            union(i_set, j_set, subsets)
+            sum_of_weights += edge["weight"]
             placed_edges += 1
 
         if placed_edges >= edges_in_tree:
             break
 
-    return minimal_spanning_tree, cost
+    return minimal_spanning_tree, sum_of_weights
 
 
 if __name__ == "__main__":
